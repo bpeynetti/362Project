25c9772120f70a85f66964f1ca24edd1d8b7fe57
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2016-02-02 18:24:45 +0000

    1 bit register finished

--- Single_Cycle_files/src/register.v
+++ Single_Cycle_files/src/register.v
@@ -1,4 +1,4 @@
-module register1(inData, clk, writeEnable, outData);
+module register1(inData, clk, writeEnable, reset, outData);
     
     input inData, clk, writeEnable;
     output outData;
@@ -7,8 +7,28 @@ module register1(inData, clk, writeEnable, outData);
     wire muxOut;
     
     mux_1 CHECK_WRITE(ffOut, inData, writeEnable, muxOut);
-    dff STORE_DATA(muxOut, clk, 1'b1, ffOut);
+    dff STORE_DATA(muxOut, clk, reset, ffOut);
     assign outData = ffOut;
 
 endmodule
 
+module register32(inData, clk, writeEnable, reset, outData);
+
+    input clk, writeEnable, reset;
+    input [0:31] inData;
+    output [0:31] outData;
+    
+    genvar i;
+    generate
+        for (i=0; i<32; i=i+1) begin: 32BIT_REGISTER
+            register1 REGISTER1 (
+                .inData(inData[i]),
+                .clk(clk),
+                .writeEnable(writeEnable),
+                .reset(reset),
+                .outData(outData[i]);
+        end
+    endgenerate
+
+endmodule
+    
