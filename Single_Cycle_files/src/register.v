7778601154a315e57d1e23b461b1e38702589e9e
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2016-02-02 17:05:04 +0000

    1 bit register finished

--- Single_Cycle_files/src/register.v
+++ Single_Cycle_files/src/register.v
@@ -3,6 +3,12 @@ module register1(inData, clk, writeEnable, outData);
     input inData, clk, writeEnable;
     output outData;
     
+    wire ffOut;
+    wire muxOut;
     
+    mux_1 CHECK_WRITE(ffOut, inData, writeEnable, muxOut);
+    dff STORE_DATA(muxOut, clk, 1'b1, ffOut);
+    assign outData = ffOut;
 
 endmodule
+
