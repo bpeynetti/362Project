960e258ee57ff89a6be748546eb25dae9e570a2e
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2016-02-14 18:09:30 +0000

    control signals almost done

--- Single_Cycle_files/src/control.v
+++ Single_Cycle_files/src/control.v
@@ -27,11 +27,14 @@ module control(
     //NOTE: for I-type instructions, rd is located where rs2 is located for R-type instructions
     //This will simply be called rs2 for the purposes of our processor
     wire [0:5] opcode, func;
+    wire [0:4] fpfunc;
     wire [0:4] rs1, rs2, rd;
     wire [0:15] imm16;
     wire [0:25] imm26;
+    wire FPRType;
     assign opcode = instruction[0:5];
     assign func = instruction[26:31];
+    assign fpfunc = instruction[27:31];
     assign rs1 = instruction[6:10];
     assign rs2 = instruction[11:15];
     assign rd = instruction[16:20];
@@ -44,15 +47,16 @@ module control(
     assign branch = (~opcode[0]) & (~opcode[1]) & (~opcode[2]) & opcode[3] & (~opcode[4]);
     assign branchZero = branch & (~opcode[5]);
     assign RType = (~opcode[0]) & (~opcode[1]) & (~opcode[2]) & (~opcode[3]) & (~opcode[4]) & (~opcode[5]);
+    assign FPRType = (~opcode[0]) & (~opcode[1]) & (~opcode[2]) & (~opcode[3]) & (~opcode[4]) & opcode[5];
     assign RegWrite = 
-    assign MemToReg = opcode[0] & (~opcode[1]) & (~opcode[2]) & 
-    assign MemWrite = 
-    assign mul = 
-    assign extOp = 
-    assign LHIOp = 
+    assign MemToReg = opcode[0] & (~opcode[1]) & (~opcode[2]) & ((~opcode[4]) | ((~opcode[3]) & opcode[4] & opcode[5]));
+    assign MemWrite = opcode[0] & (~opcode[1]) & opcode[2] & (~opcode[3]) & ((~opcode[4]) | (opcode[4] & opcode[5]));
+    assign mul = FPRType & fpfunc[2] & fpfunc[3] & (~fpfunc[4]) & (fpfunc[0] ^ fpfunc[1]);
+    assign extOp = opcode[0] & opcode[1] & (~opcode[2]) & opcode[3] & (~opcode[5]); //the opposite of the opcodes for ADDUI and SUBUI, so that it is zero for these operations
+    assign LHIOp = (~opcode[0]) & (~opcode[1]) & opcode[2] & opcode[3] & opcode[4] & opcode[5];
     
-    assign DSize[0] = 
-    assign DSize[1] = 
+    assign DSize[0] = opcode[0] & (~opcode[1]) & (~opcode[3]) & opcode[4] & opcode[5];
+    assign DSize[1] = (opcode[0] & (~opcode[1]) & (~opcode[3]) & opcode[5]) | (opcode[0] & (~opcode[1]) & (~opcode[2]) & opcode[3] & (~opcode[4]) & opcode[5]);
     
     assign ALUCtrl[0] = 
     assign ALUCtrl[1] = 
